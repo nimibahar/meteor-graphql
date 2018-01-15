074d1494dd0b79dd16939891d55e1a9a99b0f4a8
Nimrod Bahar <nimrodbahar@gmail.com>
2018-01-15 07:55:34 +0200

    running a first graphql query

--- apollo/imports/startup/server/index.js
+++ apollo/imports/startup/server/index.js
@@ -1,4 +1,30 @@
 import { createApolloServer } from 'meteor/apollo';
-import { makeExecutable } from 'graphql-tools';
+import { makeExecutableSchema } from 'graphql-tools';
 
-createApolloServer({});
+
+//all of our graphql schemas
+const typeDefs = `
+type Query {
+  hi: String
+}
+`;
+
+
+//Js resolvers
+const resolvers = {
+  Query: {
+    hi() {
+      return "Hello GraphQL"
+    }
+  }
+}
+
+//connect the query and the resolvers (makeExecutable is accpeting those)
+const schema = makeExecutableSchema({
+  typeDefs,
+  resolvers
+});
+
+
+//paste it in the Apollo server
+createApolloServer({ schema });
